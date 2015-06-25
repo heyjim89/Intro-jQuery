5720724c20600b3991896267ba6cecc4c2a74c8a
Jim D <heyjim89@gmail.com>
2015-06-25 10:43:25 -0600

    Final commit

--- main.js
+++ main.js
@@ -5,7 +5,38 @@
 // 	\____/\___\_\____/_____/_/ |_| /_/  /___/_/ |_/ /_/ /_/ |_|\____/  
 // 	                                                                   
 
-$(document).ready(function() {
+// console.log($('h1'));
+
+// $('body') // Element tag selector
+// $('.some-class') //Class selector
+// $('#my-id') //ID selector
+
+// $('#my-id > p') // Child selector
+// $('.some-class span') //Descendent selector
+
+// $('.some-class, .some-other-class') //Multi selector
+
+// // console.log($('.box'))
+// // var myBox = $('.box');
+
+// var $myBox = $('.box');
+
+// console.log('Width: ' + $myBox.width());
+// console.log('Height: ' + $myBox.height()); //without arguments just display
+
+// $myBox.height(800)  // With arguments its changing height
+
+// console.log('Height with height() change: ' + $myBox.height());
+
+// $myBox.css('height');
+// $myBox.css('height', '600px');
+
+// console.log('Height with .css change: ' + $myBox.height());
 
 
-});
+// $myBox.css({
+// 	height : '400px',
+// 	'background-color' : 'blue'
+// });
+
+// $('.box').hide();
